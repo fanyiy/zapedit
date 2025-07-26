1c1851f74aca5e32128a72d17484f11aae75a99f
Evan Yang <evanyang1026@gmail.com>
2025-07-26 10:26:58 +0800

    Update example image size

--- app/SampleImages.tsx
+++ app/SampleImages.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 const sampleImages = [
   {
     url: "https://i.ibb.co/LdNQT9r4/image.png",
-    width: 669,
-    height: 892,
+    width: 1012,
+    height: 674,
   },
   {
     url: "https://images.unsplash.com/photo-1751210392423-d8988823cb6d?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
